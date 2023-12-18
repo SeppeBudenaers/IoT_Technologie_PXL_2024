c2988c20367e8e9e3507093f9a023dee2ff55d47
Kobe Dieryck <kobe.dieryck@icloud.com>
2023-12-18 17:47:26 +0100

    Added Show *facepalm

--- Docker/main.py
+++ Docker/main.py
@@ -1,19 +1,23 @@
 import time
 from neopixel import Neopixel
  
-numpix = 14
+numpix = 1
 pixels = Neopixel(numpix, 0, 2, "GRB")
  
 
 green = (0, 255, 0)
 blue = (0, 0, 255)
 red = (255, 0, 0)
+pixels.show()
 
  
 while True:
     pixels.fill(red)
+    pixels.show()
     time.sleep(1)
     pixels.fill(green)
+    pixels.show()
     time.sleep(1)
     pixels.fill(blue)
+    pixels.show()
     time.sleep(1)
