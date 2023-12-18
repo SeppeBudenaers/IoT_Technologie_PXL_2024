f13000b657ce554d31a10f4db518b9ec26619964
Kobe Dieryck <kobe.dieryck@icloud.com>
2023-12-18 18:10:56 +0100

    Testing LED python

--- Docker/main.py
+++ Docker/main.py
@@ -1,1 +1,19 @@
-print("test")
+import time
+from neopixel import Neopixel
+ 
+numpix = 14
+pixels = Neopixel(numpix, 0, 2, "GRB")
+ 
+
+green = (0, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
+ 
+while True:
+    pixels.fill(red)
+    time.sleep(1)
+    pixels.fill(green)
+    time.sleep(1)
+    pixels.fill(blue)
+    time.sleep(1)
