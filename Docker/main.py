48fd356bc8b5f2992322b8a448342876f42290dd
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-23 22:09:02 +0100

    testing with file IO

--- Docker/main.py
+++ Docker/main.py
@@ -2,8 +2,31 @@ import argparse
 import requests
 import time
 import os
-API = os.environ.get('API_KEY')
-print(API + " OS")
+def GetAPIKEYOS():
+    try:
+        API_KEY = os.environ.get('API_KEY')
+        return API_KEY  
+    except KeyError:
+        print(f"Enviormental Key Does not exist")
+
+def GetAPIKEYOS():
+    try:
+        parser = argparse.ArgumentParser(description='IOT program')
+        parser.add_argument('API_KEY', metavar='API_KEY',type=str, help='enter your API key')
+        args = parser.parse_args()
+        return args.API_KEY
+
+def GetAPIKEYFile(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+API_key =GetAPIKEYFile("./secretfile.txt")
 
 url = 'http://iot.pxl.bjth.xyz/api/v1/temperature' 
 data = {
@@ -16,18 +39,10 @@ data = {
 
 }
 headers = {
-    'X-Api-Key': API,
+    'X-Api-Key': API_key,
     'Content-Type': 'application/json'
 }
 
 response = requests.put(url, headers=headers, json=data)
 
 print(response)
-
-parser = argparse.ArgumentParser(description='IOT program')
-parser.add_argument('API_KEY', metavar='API_KEY',type=str, help='enter your API key')
-args = parser.parse_args()
-
-API_KEY = args.API_KEY
-
-print(API_KEY + " ARGPARSE")
