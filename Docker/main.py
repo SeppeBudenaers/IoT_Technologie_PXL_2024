4ff82e9eefce1baa75fc949cd52b5acaf13d6b88
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-23 22:32:55 +0100

    added more debug

--- Docker/main.py
+++ Docker/main.py
@@ -19,6 +19,7 @@ def GetAPIKEYFile(file_path):
     try:
         with open(file_path, 'r') as file:
             content = file.read()
+            print(content +"content")
             return content
     except FileNotFoundError:
         print(f"File not found: {file_path}")
@@ -26,7 +27,7 @@ def GetAPIKEYFile(file_path):
         print(f"An error occurred: {e}")
 
 API_KEY = GetAPIKEYFile("./secretfile.txt")
-print(API_KEY)
+print(API_KEY + "APIKEY")
 
 url = 'http://iot.pxl.bjth.xyz/api/v1/temperature' 
 data = {
