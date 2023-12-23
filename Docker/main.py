7effcd5f723fa9f8e637e221b442d8bda48b70f3
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-23 21:42:20 +0100

    first test

--- Docker/main.py
+++ Docker/main.py
@@ -1,7 +1,28 @@
 import argparse 
+import requests
+import time
 import os
 API = os.environ.get('API_KEY')
 print(API + " OS")
+
+url = 'http://iot.pxl.bjth.xyz/api/v1/temperature' 
+data = {
+
+ "id": time.time(),
+
+ "value": 25.5,
+
+ "scale": "F"
+
+}
+headers = {
+    'X-Api-Key': API,
+    'Content-Type': 'application/json'
+}
+
+response = requests.put(url, headers=headers, json=data)
+
+print(response)
 
 parser = argparse.ArgumentParser(description='IOT program')
 parser.add_argument('API_KEY', metavar='API_KEY',type=str, help='enter your API key')
