c64a96dd5796f96f7fe1e45e28163d2b434bcdf2
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-23 21:11:56 +0100

    used OS again

--- Docker/main.py
+++ Docker/main.py
@@ -1,4 +1,7 @@
 import argparse 
+import os
+API = os.environ.get('API')
+print(str(API) + " OS")
 
 parser = argparse.ArgumentParser(description='IOT program')
 parser.add_argument('API_KEY', metavar='API_KEY',type=str, help='enter your API key')
@@ -6,4 +9,4 @@ args = parser.parse_args()
 
 API_KEY = args.API_KEY
 
-print(API_KEY + "ARGPARSE")
+print(API_KEY + " ARGPARSE")
