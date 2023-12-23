7ac51baba68a5f4ca2b335822de3b21aaccfb0d3
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-23 17:38:10 +0100

    trying to read secret file with python

--- Docker/main.py
+++ Docker/main.py
@@ -1,1 +1,12 @@
- print("Hey i am having a mental break down help?")
+def read_and_print_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+file_path = './secretfile.txt'
+read_and_print_file(file_path)  
