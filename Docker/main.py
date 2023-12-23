08ce058d23df78b8894dddb6df13a7dd31b946e8
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-23 20:51:54 +0100

    testing diffrent library

--- Docker/main.py
+++ Docker/main.py
@@ -1,4 +1,7 @@
 import argparse 
+import os
+secret = os.environ.get('API_KEY')
+print(secret + "OS")
 
 parser = argparse.ArgumentParser(description='IOT program')
 parser.add_argument('API_KEY', metavar='API_KEY',type=str, help='enter your API key')
@@ -6,4 +9,4 @@ args = parser.parse_args()
 
 API_KEY = args.API_KEY
 
-print(API_KEY)
+print(API_KEY + "ARGPARSE")
