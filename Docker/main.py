7f013e552b419695ff71ebe67c4bf0d4c9803786
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-26 18:31:23 +0100

    trying wiring pi
    maybe we can do spi if gpio works

--- Docker/main.py
+++ Docker/main.py
@@ -1,5 +1,5 @@
 
-import RPi.GPIO as GPIO            # import RPi.GPIO module     
+import wiringpi
 from neopixel import Neopixel
 import argparse
 import requests
@@ -32,8 +32,9 @@ def GetAPIKEYFile(file_path):
         print(f"An error occurred: {e}")
 
 # GPIO Init 
-GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output   
+ledPin = 5
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(ledPin,1)   
 # ws2812 init
 numpix = 5
 pixels = Neopixel(numpix, 0, 15, "GRB")
@@ -62,13 +63,9 @@ try:
         }
         response = requests.put(url, json=data, headers=headers)
         print(response)
-        GPIO.output(24, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-        pixels.fill(255,0,0)
-        pixels.show()
+        wiringpi.digitalWrite(ledPin, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
         sleep(1)                 # wait half a second  
-        GPIO.output(24, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        pixels.fill(0,0,0)
-        pixels.show()
+        wiringpi.digitalWrite(ledPin, 0)         # set GPIO24 to 0/GPIO.LOW/False  
         sleep(1)                 # wait half a second  
   
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
