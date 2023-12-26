b42957340cd4b019e32679c139130e72261f2ce0
Kobe Dieryck <114076056+TheNeg0t1ator@users.noreply.github.com>
2023-12-26 14:51:49 +0100

    trying rpi.gpio

--- Docker/main.py
+++ Docker/main.py
@@ -1,23 +1,14 @@
-import time
-from neopixel import Neopixel
- 
-numpix = 1
-pixels = Neopixel(numpix, 0, 2, "GRB")
- 
-
-green = (0, 255, 0)
-blue = (0, 0, 255)
-red = (255, 0, 0)
-pixels.show()
-
- 
-while True:
-    pixels.fill(red)
-    pixels.show()
-    time.sleep(1)
-    pixels.fill(green)
-    pixels.show()
-    time.sleep(1)
-    pixels.fill(blue)
-    pixels.show()
-    time.sleep(1)
+import RPi.GPIO as GPIO            # import RPi.GPIO module  
+from time import sleep             # lets us have a delay  
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
+GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output   
+  
+try:  
+    while True:  
+        GPIO.output(24, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        sleep(0.5)                 # wait half a second  
+        GPIO.output(24, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        sleep(0.5)                 # wait half a second  
+  
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup()                 # resets all GPIO ports used by this program  
