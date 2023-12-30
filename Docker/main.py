58c32d2d069a6df38fd3fc7984863f0377d23be7
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2023-12-30 16:58:27 +0100

    Update main.py

--- Docker/main.py
+++ Docker/main.py
@@ -1,25 +1,10 @@
 from led import RGBdata, Neopixel
-# import wiringpi
-import spidev
 import argparse
 import requests
+import json
 import time
 from time import sleep
 import os
-
-def GetAPIKEYOS():
-    try:
-        print(os.environ)
-        API_KEY = os.environ.get('API_KEY')
-        return API_KEY
-    except KeyError:
-        print("Environmental Key Does not exist")
-
-def GetAPIKEYARG():
-    parser = argparse.ArgumentParser(description='IOT program')
-    parser.add_argument('API_KEY', metavar='API_KEY', type=str, help='enter your API key')
-    args = parser.parse_args()
-    return args.API_KEY
 
 def GetAPIKEYFile(file_path):
     try:
@@ -31,70 +16,32 @@ def GetAPIKEYFile(file_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# GPIO Init 
-# ledPin = 5
-# wiringpi.wiringPiSetup()
-# wiringpi.pinMode(ledPin,1) 
-#SPI / ws2812 init
-spi = spidev.SpiDev()
-spi.open(0,0) # open /dev/spidev0.0
-spi.mode = 0b00
-spi.max_speed_hz = 6000000*2
-
 leds = Neopixel(1)
+leds.fill(RGBdata(300,200,0,100))
+print(leds.colors()) 
 
-# Choose one of the methods to get API_KEY
-# API_KEY = GetAPIKEYOS()
-# print(API_KEY)
-# API_KEY = GetAPIKEYARG()
-# print(API_KEY)
-# API_KEY = GetAPIKEYFile("secretfile.txt")
-# print(API_KEY)
-
-# url = 'http://iot.pxl.bjth.xyz'
-
-# headers = {
-#     '-h':"X-Api-Key: "+str(API_KEY)
-# }
+API_KEY = GetAPIKEYFile("secretfile.txt")
+print(API_KEY)
+url = 'http://iot.pxl.bjth.xyz/api/v1/LED'
+headers = {
+    'X-Api-Key': str(API_KEY)  # Fix the header format
+}
 
 # main loop
-try:  
-    # for i in  range (0,10):
-    #     data = {
-    #         "id": time.time(),
-    #         "value": 25.5,
-    #         "scale": "F"
-    #     }
-    #     response = requests.put(url, json=data, headers=headers)
-    #     print(response)
-    #     # wiringpi.digitalWrite(ledPin, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-    #     sleep(1)                 # wait half a second  
-    #     # wiringpi.digitalWrite(ledPin, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-    #     sleep(1)                 # wait half a second
-    #print(bin(leds.outputData()))
-    buf = bytes(leds.ws2812_Data())
-    print(buf)
-    spi.writebytes2(buf)
-    time.sleep(1)
+data = {
+    "R": 255,
+    "G": 0,
+    "B": 255,
+    "Brightness": 255
+}
+RGBSend = requests.put(url, json=data, headers=headers)
+if RGBSend.status_code != 200:
+    print("error : "+ str(RGBSend.status_code))
 
-    leds.fill(RGBdata(255,0,0,255))     
-    buf = bytes(leds.ws2812_Data())
-    print(buf)
-    spi.writebytes2(buf)
-    time.sleep(3)
+RGBRecieve = requests.get(url, headers=headers)
+if RGBRecieve.status_code != 200:
+    print("error : "+ str(RGBRecieve.status_code))
 
-    leds.fill(RGBdata(0,0,255,255))
-    buf = bytes(leds.ws2812_Data())
-    print(buf)
-    spi.writebytes2(buf)
-    time.sleep(3)
-
-    leds.fill(RGBdata(0,255,0,255))
-    buf = bytes(leds.ws2812_Data())
-    print(buf)
-    spi.writebytes2(buf)
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-    GPIO.cleanup()                 # resets all GPIO ports used by this program
-
-
-
+LED = json.loads(RGBRecieve.json())
+leds.fill(RGBdata(LED['R'],LED['G'],LED['B'],LED['Brightness']))
+print(leds.colors()) 
