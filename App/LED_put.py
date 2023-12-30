3045f5c53c455a8c063a93258676e69e4b7641db
Kobe Dieryck <114076056+TheNeg0t1ator@users.noreply.github.com>
2023-12-30 21:55:29 +0100

    Added a script to run docker and edited led_put.py

--- App/LED_put.py
+++ App/LED_put.py
@@ -1,23 +1,36 @@
-from led import RGBdata, Neopixel
-import argparse
 import requests
-import json
-import time
-from time import sleep
-import os
+import argparse
 
-API_KEY = #your API key
+def GetAPIKEYFile(file_path:str):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+parser = argparse.ArgumentParser(description='IOT program')
+parser.add_argument('RED', metavar='RED', type=int, help='enter red')
+parser.add_argument('GREEN', metavar='GREEN', type=int, help='enter green')
+parser.add_argument('BLUE', metavar='BLUE', type=int, help='enter blue')
+parser.add_argument('BRIGHTNESS', metavar='BRIGHTNESS', type=int, help='enter Brightness')
+args = parser.parse_args()
+API_KEY = GetAPIKEYFile("apikey.txt")
 url = 'http://iot.pxl.bjth.xyz/api/v1/LED'
 headers = {
     'X-Api-Key': str(API_KEY)  # Fix the header format
 }
 
+
+
 # main loop
 data = {
-    "R": 255,
-    "G": 255,
-    "B": 0,
-    "Brightness": 255
+    "R": args.RED,
+    "G": args.GREEN,
+    "B": args.BLUE,
+    "Brightness": args.BRIGHTNESS
 }
 RGBSend = requests.put(url, json=data, headers=headers)
 if RGBSend.status_code != 200:
